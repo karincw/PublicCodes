89dda8be5fb7d9f064eb84eb33367b34d33c1a90
Karin <99000645+karincw@users.noreply.github.com>
2025-04-13 12:09:35 +0900

    Update Sorting.cs

--- MusicOfDefence/Instrument/Sorting.cs
+++ MusicOfDefence/Instrument/Sorting.cs
@@ -22,64 +22,63 @@ public class Sorting : MonoBehaviour
     {
         MaxButton = Mathf.Clamp(MaxButton, MaxLine, MaxLine * 4);
     }
+
     public void HorizontalSort()
     {
-        bool secondLine = false;
         if (btns.Count == _instrumentManager.noteBtns.Count)
-        {
             btns = _instrumentManager.noteBtns;
-        }
-        xSize = (float)MaxLine / btns.Count;
-        xSize = Mathf.Clamp(xSize, 0.5f, 1);
+    
+        xSize = Mathf.Clamp((float)MaxLine / btns.Count, 0.5f, 1);
+        int lineChange = MaxLine * 2;
+        float baseY = transform.position.y;
+        float posX = sortStartPos + halfX;
 
+        List<Btn> firstLineBtns = new List<Btn>();
+        List<Btn> secondLineBtns = new List<Btn>();
+    
+        for (int i = 0; i < btns.Count; i++)
         {
-            int lineChange = MaxLine * 2;
-            int i = 0;
-            float posY1 = transform.position.y;
-            float posY2 = transform.position.y;
-            float posX = sortStartPos + halfX;
-            bool secondLineSorting = false;
-            List<Btn> sortingBtns = new List<Btn>();
-            foreach (var btn in btns)
+            if (i < lineChange)
+                firstLineBtns.Add(btns[i]);
+            else
+                secondLineBtns.Add(btns[i]);
+        }
+
+        // First Line
+        for (int i = 0; i < firstLineBtns.Count; i++)
+        {
+            Vector2 pos = new Vector2(posX + (xSize * i), baseY);
+            Vector3 scale = new Vector3(xSize, 1, 1);
+            SetBtnPositionAndScale(firstLineBtns[i], pos, scale);
+        }
+
+        // Second Line (if needed)
+        if (secondLineBtns.Count > 0)
+        {
+            float upperY = baseY + halfX;
+            float lowerY = baseY - halfX;
+
+            for (int i = 0; i < firstLineBtns.Count; i++)
             {
-                sortingBtns.Add(btn);
-                if (i >= lineChange)
-                {
-                    secondLine = true;
-                    posY1 = transform.position.y + halfX;
-                    posY2 = transform.position.y - halfX;
-                    posX = sortStartPos + halfX;
-                }
+                Vector2 pos = new Vector2(posX + (xSize * i), upperY);
+                Vector3 scale = new Vector3(xSize, xSize, 1);
+                SetBtnPositionAndScale(firstLineBtns[i], pos, scale);
+            }
 
-                if (secondLine)
-                {
-                    if (secondLineSorting == false)
-                    {
-                        int i2 = 0;
-                        foreach (var btn2 in sortingBtns)
-                        {
-                            btn2.transform.position = new Vector2(posX + (xSize * i2), posY1);
-                            btn2.transform.localScale = new Vector3(xSize, xSize, 1);
-                            btn2.ResetOrigin();
-                            i2++;
-                        }
-                        secondLineSorting = true;
-                        i = 0;
-                    }
-                    btn.transform.localScale = new Vector3(xSize, xSize, 1);
-                    btn.transform.position = new Vector2(posX + (xSize * i), posY2);
-                    btn.ResetOrigin();
-                }
-                else
-                {
-                    btn.transform.position = new Vector2(posX + (xSize * i), transform.position.y);
-                    btn.transform.localScale = new Vector3(xSize, 1, 1);
-                    btn.ResetOrigin();
-                }
-
-                i++;
+            for (int i = 0; i < secondLineBtns.Count; i++)
+            {
+                Vector2 pos = new Vector2(posX + (xSize * i), lowerY);
+                Vector3 scale = new Vector3(xSize, xSize, 1);
+                SetBtnPositionAndScale(secondLineBtns[i], pos, scale);
             }
         }
+    }
+
+    private void SetBtnPositionAndScale(Btn btn, Vector2 position, Vector3 scale)
+    {
+        btn.transform.position = position;
+        btn.transform.localScale = scale;
+        btn.ResetOrigin();
     }
 
     public void Dimension2Sort()
@@ -105,10 +104,9 @@ public class Sorting : MonoBehaviour
                     posX = sortStartPos + halfX;
                     lineChange += MaxLine;
                 }
-                btn.gameObject.transform.position = new Vector2(posX++, posY);
-                btn.gameObject.transform.localScale = new Vector3(xSize, 1, 1);
-                btn.ResetOrigin();
-
+                Vector2 pos = new Vector2(posX++, posY);
+                Vector3 scale = new Vector3(xSize, 1, 1);
+                SetBtnPositionAndScale(btn, pos, scale);
                 i++;
             }
         }
